0d526bf4a9b33cdf200f2a4335314509b8d0ad26
Malik Ahmad Mukhtar <158511475+malikahmadmukhtar@users.noreply.github.com>
2024-02-14 01:00:46 +0500

    remove firebase options from main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -8,12 +8,9 @@ void main() async {
   Firestore.initialize("shopping-zone-36ab7");
   await Firebase.initializeApp(
       options: const FirebaseOptions(
-        apiKey: "AIzaSyB_ToipEfexJKHE0jR-FKuo9iMbHtSWsV4",
-        appId: "1:121390676895:web:38c2d8c1f7aa087a45dbaa",
-        messagingSenderId: "121390676895",
-        storageBucket: "shopping-zone-36ab7.appspot.com",
-        //databaseURL: "https://pharmacy-app-decf9-default-rtdb.firebaseio.com",
-        projectId: "shopping-zone-36ab7",));
+        //add your firebase api here
+        )
+  );
   runApp(const MyApp());
 }
 
